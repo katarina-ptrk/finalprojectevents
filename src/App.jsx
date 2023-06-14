cf930ff21b6795537184c8c9a162fde1734b25df
Kateryna Petryk <kateryna2802@gmail.com>
2023-06-14 09:32:06 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './nav.css';
+
+const NavigationBar = () => {
+
+
+  return (
+    <div></div>
+  );
+};
+
+export default NavigationBar;
