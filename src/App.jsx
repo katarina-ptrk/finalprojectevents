ce2a088fffa6e25a4db9b2efd07ff8365beac4ed
Kateryna Petryk <kateryna2802@gmail.com>
2023-06-18 20:36:49 +0200

    route completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from "./pages/Homepage";
 import './nav.css';
+import Create from './pages/Create';
+import Event from './pages/Event';
+import Events from './pages/Events';
+import JoinEvent from './pages/JoinEvent';
+import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
+import Preview from './pages/Preview';
+import UserPage from './pages/UserPage';
 
-const NavigationBar = () => {
 
+
+
+function App() {
 
   return (
-    <div></div>
-  );
-};
+    <>
+      <Routes>
+        <Route path="/" element={<Homepage />}>
+        </Route>
+        <Route path="/create" element={<Create />}>
+        </Route>
+        <Route path="/event" element={<Event />}>
+        </Route>
+        <Route path="/events" element={<Events />}>
+        </Route>
+        <Route path="/joinevent" element={<JoinEvent />}>
+        </Route>
+        <Route path="/login" element={<LogIn />}>
+        </Route>
+        <Route path="/signup" element={<SignUp />}>
+        </Route>
+        <Route path="/preview" element={<Preview />}>
+        </Route>
+        <Route path="/userpage" element={<UserPage />}>
+        </Route>
+      </Routes>
+    
+    </>
+  )
+}
 
-export default NavigationBar;
+export default App
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div>
+        <a href="/"><img src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6918e57475a843f59f_icon_clyde_black_RGB.svg" alt="" /></a>
+        <div className="avatar">
+  <div className="w-24 rounded-full">
+    <img src="https://i.pinimg.com/236x/99/2a/b0/992ab03eccadc497440f66e99bfac2e7.jpg" />
+  </div>
+</div> 
+   </div>
+  )
+}
+
+export default Homepage
